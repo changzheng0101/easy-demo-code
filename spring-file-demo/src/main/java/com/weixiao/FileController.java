390833abf2c4d02eaf0ea7030fd4c4146aabce51
changzheng.15 <changzheng.15@jd.com>
2025-11-03 14:34:53 +0800

    下载文件

--- spring-file-demo/src/main/java/com/weixiao/FileController.java
+++ spring-file-demo/src/main/java/com/weixiao/FileController.java
@@ -1,12 +1,22 @@
 package com.weixiao;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Arrays;
 
 /**
  * @author changzheng
@@ -27,7 +37,7 @@ public class FileController {
     @RequestMapping("/normalFile")
     public String normal(@RequestParam("file") MultipartFile file) {
         // Setting up the path of the file
-        String filePath = resourcePath  +File.separator + file.getOriginalFilename();
+        String filePath = resourcePath + File.separator + file.getOriginalFilename();
         String fileUploadStatus;
 
         File destFile = new File(filePath);
@@ -71,9 +81,33 @@ public class FileController {
      *
      * @return
      */
-    @RequestMapping("/downlaodFile")
-    public String downloadFile() {
-        return "downloadFile";
+    @RequestMapping("/downloadFile/{path:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable("path") String filename) {
+        // Checking whether the file requested for download exists or not
+        String[] filenames = this.getFiles();
+        boolean contains = Arrays.asList(filenames).contains(filename);
+        if (!contains) {
+            return new ResponseEntity("file Not Found", HttpStatus.NOT_FOUND);
+        }
+
+        // Setting up the filepath
+        String filePath = resourcePath + File.separator + filename;
+
+        // Creating new file instance
+        File file = new File(filePath);
+        InputStreamResource resource = null;
+        try {
+            resource = new InputStreamResource(new FileInputStream(file));
+        } catch (FileNotFoundException e) {
+            return new ResponseEntity("file Not Found", HttpStatus.NOT_FOUND);
+        }
+        String contentType = "application/octet-stream";
+        String headerValue = "attachment; filename=\"" + resource.getFilename() + "\"";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, headerValue)
+                .body(resource);
     }
 
     @RequestMapping(value = "/getFiles")
